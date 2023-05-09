cd658da0c4ab451d8a3972839fc9c03d0923df5b
unknown <thomasjjohnson9311@gmail.com>
2023-05-08 23:15:20 -0500

    tables

--- schema.sql
+++ schema.sql
@@ -1,12 +1,31 @@
-DROP DATABASE IF EXISTS test;
-CREATE DATABASE test;
+DROP DATABASE IF EXISTS corp_db
+CREATE DATABASE corp_db
+USE corp_db;
 
-USE test;
-
-CREATE TABLE example (
+CREATE TABLE department (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+    name VARCHAR(30)
 );
 
-INSERT INTO example (name)
-VALUES ("John"), ("Jacob"), ("Mary");
+CREATE TABLE roles {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) -- 30 char max
+    salary DECIMAL (10, 2) -- ten digits before the decimal and two after
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+
+}
+
+CREATE TABLE employee {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+
+}
---
+++ seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name)
+VALUES("Accounting"),
+("Human Resources"),
+("Marketing"),
+("Legal"),
+("Sales");
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Lead Accountant", 123000.00, 1)
+("Accounting Assistant", 65000.00, 1)
+("HR Director", 110000.00, 2)
+("HR Assistant", 45000.00, 2)
+("Advertising Manager", 98000.00, 3)
+("Marketing Manager", 95000.00, 3)
+("Legal Team Lead", 175000.00, 4)
+("Lawyer", 140000.00, 4)
+("Lead salesperson", 120000.00, 5)
+("Salesperson", 75000.00, 5)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+
