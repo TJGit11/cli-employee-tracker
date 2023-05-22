60268ba4d41e317291598482e04fb153eabb1370
unknown <thomasjjohnson9311@gmail.com>
2023-05-21 23:21:14 -0500

    viewAllEmployees prompt works correctly

--- corpFunc.mjs
+++ corpFunc.mjs
@@ -12,7 +12,9 @@ import { promisePool } from "./utils/pool.mjs";
 //         ])
 
 export async function viewAllEmployees() {
-  const [rows] = await promisePool.query("SELECT * FROM employee");
+  const [rows] = await promisePool.query(
+    'SELECT employee.id, employee.first_name, employee.last_name, roles.title, department.name AS department, roles.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id'
+  );
   console.table(rows);
 }
 
--- index.mjs
+++ index.mjs
@@ -4,61 +4,38 @@ import { promisePool } from "./utils/pool.mjs";
 import {
   viewAllEmployees,
   addEmployee,
-  updateEmployeeRole,
   viewAllRoles,
   addRole,
-  // viewAllDepartments,
-  addDepartment,
+  updateEmployeeRole,
   viewAllDepartments,
+  addDepartment,
 } from "./corpFunc.mjs";
 
-// viewAllEmployees;
+async function begin() {
+  const answers = await inquirer.prompt({
+    message: "What would you like to do?",
+    type: "list",
+    name: "options",
+    choices: [
+      "View all employees",
+      "Add an employee",
+      "View all employee roles",
+      "Add a new employee role",
+      "Update an employee role",
+      "View all departments",
+      "Add a new department",
+    ],
+  });
 
-// import corpFunc from "./corpFunc.mjs";
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "corp_db",
-//   password: "rootroot",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 10, // max idle connections, the default value is the same as 'connectionLimit'
-//   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value is 60000
-//   queueLimit: 0,
-// });
-
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   database: "corp_db",
-//   password: "rootroot",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   maxIdle: 10, // max idle connections, the default value is the same as 'connectionLimit'
-//   idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value is 60000
-//   queueLimit: 0,
-// });
-viewAllEmployees();
-viewAllRoles();
-viewAllDepartments();
-// inquirer prompt - check bootcamp for example
-// let { firstName } = await inquirer.prompt([
-//   {
-//     type: "input",
-//     name: "firstName",
-//   },
-// ]);
-// const promisePool = pool.promise();
-// async function viewAllDepartments() {
-//   try {
-//     const [rows] = await pool.query("SELECT * FROM department");
-//     console.table(rows);
-//     return rows;
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
+  if (answers.options === "View all employees") {
+    viewAllEmployees();
+  }
+  // begin();
+}
+begin();
+// viewAllEmployees();
+// viewAllRoles();
+// viewAllDepartments();
 
 // const promisePool = pool.promise();
 // const [rows] = await promisePool.query(
